8bbf7cf38919f6a9a67462781c8e58cbc7dbdd3a
Meredith Plumley <meredith.plumley@colorado.edu>
2018-11-27 10:18:15 +0100

    updated example_run.py - MP

--- example_run.py
+++ example_run.py
@@ -1,19 +1,24 @@
-from BaseReader import BaseState, ConvertedState, UnconvertedState
+from BaseReader import BaseState, SpectralState, PhysicalState
 
 # This should be the path to the state file
 filename = 'PATH_TO_STATE_FILE'
+filename = '/Users/meredith/Desktop/state_convert.hdf5'
 
 # Use either SpectralState() or PhysicalState() depending on the file you are reading
-a = ConvertedState()
+a = PhysicalState()
 
 a.read(filename)
 
 # Read time and timestep 
-a.parameters.time
-a.parameters.timestep
+time = a.parameters.time
+print('time: ', time)
+timestep = a.parameters.timestep
+print('timestep: ', timestep)
 
 # Read problem specific parameters from: a.parameters.PARAM_OF_CHOICE
 # Example:  a.parameters.ekman, a.parameters.omega
+# Ra = a.parameters.rayleigh
+# print('Ra: ', Ra)
 
 # Read mesh from a.parameters.GRID_OPTIONS
 # Example:  a.parameters.grid_x, a.parameters.grid_r
