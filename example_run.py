0e3e7e3fa162f76e2ac32e8850d9e16c10591fe4
lardellin <lardellin@users.noreply.github.com>
2018-12-03 14:33:00 +0100

    Update example_run.py

--- example_run.py
+++ example_run.py
@@ -1,11 +1,12 @@
-from BaseReader import BaseState, SpectralState, PhysicalState
+from BaseReader import SpectralState, PhysicalState
 
 # This should be the path to the state file
-filename = 'PATH_TO_STATE_FILE'
+# e.g.
+#filename = 'PATH_TO_STATE_FILE'
 filename = '/Users/meredith/Desktop/state_convert.hdf5'
 
 # Use either SpectralState() or PhysicalState() depending on the file you are reading
-a = PhysicalState()
+my_state_physical = PhysicalState(filename)
 
 #Leo: change this to 
 #Add examples for every case
@@ -14,24 +15,23 @@ a = PhysicalState()
 #Cartesian: 
 #TODO: See if it's possible to truncate the spectral resolution for the PhysicalState?
 #Ultimately, for a publication one will provide a Python Notebook to generate all the figures
-#state=PhysicalState(filename)
-
-a.read(filename)
+filename = './state0000.hdf5'
+#my_state_spectral=SpectralState(filename)
 
 # Read time and timestep 
-time = a.parameters.time
+time = my_state.parameters.time
 print('time: ', time)
-timestep = a.parameters.timestep
+timestep = my_state.parameters.timestep
 print('timestep: ', timestep)
 
 # Read problem specific parameters from: a.parameters.PARAM_OF_CHOICE
-# Example:  a.parameters.ekman, a.parameters.omega
-# Ra = a.parameters.rayleigh
+# Example:  my_state_spectral.parameters.ekman, my_state_spectral.parameters.omega
+# Ra = my_state_physical.parameters.rayleigh
 # print('Ra: ', Ra)
 
 # Read mesh from a.parameters.GRID_OPTIONS
-# Example:  a.parameters.grid_x, a.parameters.grid_r
+# Example:  my_state_physical.parameters.grid_x, my_state_physical.parameters.grid_r
 
-# Read fields from a.fields.FIELDS
-# Example:  a.fields.velocityz, a.fields.velocity_tor
+# Read fields from my_state_physical.fields.FIELDS
+# Example:  my_state_physical.fields.velocityz, my_state_spectral.fields.velocity_tor
 
