a9e8e60a3c28f1475c2905c95576e3eaf179aed7
lardellin <lardellin@users.noreply.github.com>
2018-12-03 14:51:29 +0100

    Update BaseReader.py
    pushing changes BaseReader on nicolol side

--- BaseReader.py
+++ BaseReader.py
@@ -4,12 +4,15 @@ import numpy as np
     
 class BaseState:
     
-    def __init__(self, filename):
-    
+    def __init__(self, filename, geometry, file_type='QuICC'):
+        
+        # PhysicalState('state0000.hdf5',geometry='Sphere'
         fin = h5py.File(filename, 'r')
         self.fin = fin
 
         # TODO: distinguish between EPM and QuICC
+        if file_type.lower()!='quicc':
+            self.isEPM = True
         # assuming using QuICC state syntax
         
         self.dtype='QuICC'
@@ -29,7 +32,9 @@ class BaseState:
             
             setattr(self.parameters, at, fin['physical'][at].value)
         
-        self.geometry = fin.attrs['type']
+        #self.geometry = fin.attrs['type']
+        self.geometry = geometry
+        # geometry so far is
     pass
 
 class PhysicalState(BaseState):
