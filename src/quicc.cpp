ae49269b438084d53be33b560915b9feee00c1dc
Leo <leonardo.echeverria@erdw.ethz.ch>
2019-02-14 17:02:59 +0100

    adding associate legendre poly in c++

--- src/quicc.cpp
+++ src/quicc.cpp
@@ -1,4 +1,16 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+#include <Eigen/LU>
 #include <iostream>
+#include "PolynomialTransforms/AssociatedLegendrePolynomial.hpp"
+
+//Associated legendre polynomial
+void plm( Eigen::Ref<QuICC::Matrix> op, int m, QuICC::Array igrid){
+    QuICC::Matrix mOp(op);
+    QuICC::Matrix ipoly(op);
+    QuICC::Polynomial::AssociatedLegendrePolynomial::Plm(mOp, ipoly, m, igrid);
+    op = mOp;
+}
 
 int main(){
 return 0;
